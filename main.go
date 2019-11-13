e399453c04351ac58186294f993af72283b445ab
moko <momoko@shinbanmomokonoMacBook-Pro.local>
2019-11-13 19:03:11 +0900

    complete

--- main.go
+++ main.go
@@ -3,21 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	var annualIncome int
 	var days int
 	var sundays int
+	var workingHours int
+	var workingMinute int
+	fmt.Print("時給を入れてください:")
+	fmt.Scan(&annualIncome)
 	fmt.Print("勤務日数を入力してください：")
 	fmt.Scan(&days)
 	fmt.Print("日曜日の日数を入力してください：")
 	fmt.Scan(&sundays)
-	annualIncome := 1030
-	b := annualIncome + 30
-	a := 1030 / 4 * 3
-	c := b / 4 * 3
-	workingHours := 5
-	dailySalary := annualIncome*workingHours + a
-	d := b*workingHours + c
+	fmt.Print("何時間働きますか：")
+	fmt.Scan(&workingHours)
+	fmt.Print("何分働きますか")
+	fmt.Scan(&workingMinute)
+	minuteSalary := annualIncome / 60 * workingMinute
+	sundayHourlyWage := annualIncome + 30
+	sundayMinuteSalary := sundayHourlyWage / 60 * workingMinute
+	//hourlyWage := 1013 / 4 * 3
+	//c := sundaySalary / 4 * 3
+	//workingHoursOfTheDay := workingHours + workingHours
+	dailySalary := annualIncome * workingHours + minuteSalary
+	sundaySalary := sundayHourlyWage * workingHours + sundayMinuteSalary
 	weekdays := days - sundays
 	income := dailySalary * weekdays
-	e := d*sundays + income
-	fmt.Println(e)
+	salaryForOneMonth := sundaySalary*sundays + income
+	fmt.Println(salaryForOneMonth)
 }
